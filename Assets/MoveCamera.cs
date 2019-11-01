42c9e80882f5af3bdf43148fae64005d517b8209
KReguieg <Khaled.Reguieg@gmail.com>
2019-11-01 12:54:32 +0100

    [ADD] Move camera with keyboard and [EDIT] example scene. :)

---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+            transform.position += Vector3.left;
+        if (Input.GetKeyDown(KeyCode.W))
+            transform.position += Vector3.up;
+        if (Input.GetKeyDown(KeyCode.S))
+            transform.position += Vector3.back;
+        if (Input.GetKeyDown(KeyCode.D))
+            transform.position += Vector3.right;
+    }
+}
